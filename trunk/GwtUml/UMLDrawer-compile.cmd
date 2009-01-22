b7dc4c6b35632d5b750e8aace04e93a9ebc5753f
mounier.florian <mounier.florian@ac043e3a-cdac-11dd-a2c4-6dc3ad32ce87>
2009-01-22 13:14:58 +0000

    Added latest_build directory + automatic update

--- trunk/GwtUml/UMLDrawer-compile.cmd
+++ trunk/GwtUml/UMLDrawer-compile.cmd
@@ -3,4 +3,8 @@ java ^
 -cp "%~dp0\src;%~dp0\bin;C:/gwt/gwt-user.jar;C:/gwt/gwt-dev-windows.jar;C:/gwt-tatami/tatami-1.3.jar;C:/gwt-incubator/gwt-incubator.jar;C:/gwt-log/gwt-log-2.5.3.jar" ^
 com.google.gwt.dev.GWTCompiler ^
 -out "%~dp0\www" %* com.objetdirect.gwt.umlapi.UMLDrawer
-xcopy www\com.objetdirect.gwt.umlapi.UMLDrawer latest_build \s 
+c:\svn\bin\svn.exe rm latest_build\*.html
+c:\svn\bin\svn.exe rm latest_build\*.rpc
+c:\svn\bin\svn.exe rm latest_build\*.png
+xcopy www\com.objetdirect.gwt.umlapi.UMLDrawer latest_build /s /d
+c:\svn\bin\svn.exe add --force latest_build\*
