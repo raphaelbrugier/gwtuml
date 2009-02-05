0ca22f6a95c8f71054d239edcb6e367228c1633d
mounier.florian <mounier.florian@ac043e3a-cdac-11dd-a2c4-6dc3ad32ce87>
2009-02-05 16:10:13 +0000

    Added umlcomponents javadoc

--- trunk/GwtUml/src/com/objetdirect/gwt/umlapi/client/umlcomponents/Attribute.java
+++ trunk/GwtUml/src/com/objetdirect/gwt/umlapi/client/umlcomponents/Attribute.java
@@ -1,41 +1,82 @@
 package com.objetdirect.gwt.umlapi.client.umlcomponents;
 
+/**
+ * This class represent an attribute in a class
+ * @author hdarmet
+ * @author fmounier
+ *
+ */
 public class Attribute extends ClassMember {
 
-	String name;
+	private String name;
+	private String type;
+	private boolean validated = true;
 
-	String type;
-
-	boolean validated = true;
-
+	/**
+	 * Constructor of the attribute
+	 * @param type Type of the attribute
+	 * @param name Name of the attribute
+	 */
 	public Attribute(String type, String name) {
 		super();
 		this.type = type;
 		this.name = name;
 	}
 
+	/**
+	 * Getter for the name
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Getter for the type
+	 * @return the type
+	 */
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * Get the validated state of the attribute
+	 * @return true if validated, false otherwise
+	 */
 	public boolean isValidated() {
 		return validated;
 	}
 
+	/**
+	 * Setter for the name
+	 * @param name to be set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
+	/**
+	 * Setter for the type
+	 * @param type to be set
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
+	
+	
+	/**
+	 * Set the validation state
+	 * @param boolean for validated validation state 
+	 */
 	public void setValidated(boolean validated) {
 		this.validated = validated;
 	}
+	
+	/**
+	 * Format a string from attribute name and type
+	 * @return the UML formatted string for attribute name and type
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		StringBuffer f = new StringBuffer();
--- trunk/GwtUml/src/com/objetdirect/gwt/umlapi/client/umlcomponents/Parameter.java
+++ trunk/GwtUml/src/com/objetdirect/gwt/umlapi/client/umlcomponents/Parameter.java
@@ -1,32 +1,63 @@
 package com.objetdirect.gwt.umlapi.client.umlcomponents;
 
+/**
+ * This class represent a parameter of a method
+ * @author fmounier
+ * @author hdarmet
+ */
 public class Parameter {
 
-	String name;
-
-	String type;
-
+	private String name;
+	private String type;
+	
+	/**
+	 * Constructor of the parameter
+	 * @param type Type of the parameter
+	 * @param name Name of the parameter
+	 */
 	public Parameter(String type, String name) {
 		super();
 		this.type = type;
 		this.name = name;
 	}
 
+	/**
+	 * Getter for the name
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Getter for the type
+	 * @return the type
+	 */
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * Setter for the name
+	 * @param name to be set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Setter for the type
+	 * @param type to be set
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
+	
+	/**
+	 * Format a string from parameter name and type
+	 * @return the UML formatted string for parameter name and type
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		StringBuffer f = new StringBuffer();
