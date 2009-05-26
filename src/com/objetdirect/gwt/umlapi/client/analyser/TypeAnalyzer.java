106c5cbed3044d7fccd6a77265ffdd3f48586926
mounier.florian <mounier.florian@ac043e3a-cdac-11dd-a2c4-6dc3ad32ce87>
2009-05-26 16:07:04 +0000

    Add licence in src files

--- src/com/objetdirect/gwt/umlapi/client/analyser/TypeAnalyzer.java
+++ src/com/objetdirect/gwt/umlapi/client/analyser/TypeAnalyzer.java
@@ -1,3 +1,25 @@
+/*
+ *    This file is part of the GWTUML project
+ *    and was written by Mounier Florian <mounier-dot-florian.at.gmail'dot'com> 
+ *    for Objet Direct <http://wwww.objetdirect.com>
+ *    
+ *    Copyright © 2009 Objet Direct
+ *    Contact: gwtuml@googlegroups.com
+ *    
+ *    GWTUML is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    GWTUML is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with GWTUML. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package com.objetdirect.gwt.umlapi.client.analyser;
 
 import com.objetdirect.gwt.umlapi.client.UMLDrawerException;
