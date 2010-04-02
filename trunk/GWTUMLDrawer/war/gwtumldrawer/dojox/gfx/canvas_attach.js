6e60e48f8fbd462475792a52906b03417a573b36
raphael.brugier <raphael.brugier@ac043e3a-cdac-11dd-a2c4-6dc3ad32ce87>
2010-04-02 09:45:00 +0000

    Update drawer with the latest update of additional packages : gwt-log, gwt-incubator and tatami-1.4

--- trunk/GWTUMLDrawer/war/gwtumldrawer/dojox/gfx/canvas_attach.js
+++ trunk/GWTUMLDrawer/war/gwtumldrawer/dojox/gfx/canvas_attach.js
@@ -1,8 +1,8 @@
-dojo.require("dojox.gfx.canvas");
+/*
+	Copyright (c) 2004-2009, The Dojo Foundation All Rights Reserved.
+	Available via Academic Free License >= 2.1 OR the modified BSD license.
+	see: http://dojotoolkit.org/license for details
+*/
 
-dojo.experimental("dojox.gfx.canvas_attach");
 
-// not implemented
-dojox.gfx.attachNode = function(){
-	return null;	// for now
-};
+dojo.require("dojox.gfx.canvas");dojo.experimental("dojox.gfx.canvas_attach");dojox.gfx.attachNode=function(){return null;};
